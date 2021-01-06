269fab81a7f6558fad6cae8b83a685980b21dc7e
Stephen Blair <stephenjohnblair@outlook.com>
2021-01-06 14:08:36 +0000

    Added sign up form with styles

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import '../styles/SignUp.css';
 
 
 
 const SignUp = () => {
     return (
         <>
-        <h1>from sign up</h1>
-
+            <div className="sign-up-form">
+                <i className="fas fa-user-plus"></i>
+                <h1>Sign Up Now</h1>
+                <form>
+                    <input type="email" className="input-box" placeholder="Your Email"/>
+                    <input type="password" className="input-box" placeholder="Your Password"/>
+                    <p><span><input type="checkbox"/></span> I agree to the terms of services</p>
+                    <button className="signup-btn">Sign Up</button>
+                    <hr/>
+                    <p className="or">or</p>
+                    <button className="facebook-btn">Sign Up with Facebook</button>
+                    <button className="twitter-btn">Sign Up with Twitter</button>
+                </form>
+            </div>
         </>
     )
 }
